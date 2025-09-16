f3fad54e199c3d4beb646a77933635cb024038a6
Chile <118934314+ChileKasoka@users.noreply.github.com>
2025-09-16 08:28:01 +0200

    Working on top-bar and header menu

--- themes/oceanwp/partials/topbar/content.php
+++ themes/oceanwp/partials/topbar/content.php
@@ -5,108 +5,26 @@
  * @package OceanWP WordPress theme
  */
 
-// Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+?>
 
-// Get the template.
-$template = get_theme_mod( 'ocean_top_bar_template' );
+<div id="top-bar-content" class="topbar-flex <?php echo esc_attr( oceanwp_topbar_content_classes() ); ?>">
 
-// Check if page is Elementor page.
-$elementor = get_post_meta( $template, '_elementor_edit_mode', true );
+	<!-- Left: Logo -->
+	<div class="topbar-logo">
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+			<img src="http://shopinzed.local/wp-content/uploads/2025/09/siz.png" 
+			     alt="sHoPiNzEd Logo" 
+			     style="max-height:60px; width:auto;" />
+		</a>
+	</div>
 
-// Get content.
-$get_content = oceanwp_topbar_template_content();
+	<!-- Right: Contact Info -->
+	<div class="topbar-right">
+		<span class="top-phone">📞 <a href="tel:+260971234567">+260 97 123 4567</a></span>
+		<span class="top-email">✉️ <a href="mailto:sales@shopinzed.com">sales@shopinzed.com</a></span>
+	</div>
 
-// Get topbar content.
-$content = get_theme_mod( 'ocean_top_bar_content' );
-$content = oceanwp_tm_translation( 'ocean_top_bar_content', $content );
-
-// Display topbar content.
-if ( ! empty( $template )
-	|| ! empty($content)
-	|| has_nav_menu( 'topbar_menu' )
-	|| is_customize_preview() ) : ?>
-
-	<div id="top-bar-content" class="<?php echo esc_attr( oceanwp_topbar_content_classes() ); ?>">
-
-		<?php
-		// Get topbar menu.
-		if ( has_nav_menu( 'topbar_menu' ) ) {
-			get_template_part( 'partials/topbar/nav' );
-		}
-		?>
-
-		<?php
-		// If template.
-		if ( ! empty( $template ) ) {
-			?>
-
-			<div id="topbar-template">
-
-				<?php
-				// If Elementor.
-				if ( OCEANWP_ELEMENTOR_ACTIVE && $elementor ) {
-
-					OceanWP_Elementor::get_topbar_content();
-
-				// If Beaver Builder.
-				} elseif ( OCEANWP_BEAVER_BUILDER_ACTIVE && ! empty( $template ) ) {
-
-					echo do_shortcode( '[fl_builder_insert_layout id="' . $template . '"]' );
-
-				// Else.
-				} else if ( class_exists( 'SiteOrigin_Panels' ) && get_post_meta( $template, 'panels_data', true ) ) {
-
-					echo SiteOrigin_Panels::renderer()->render( $template );
-
-				} else {
-
-					// If Gutenberg.
-					if ( ocean_is_block_template( $template ) ) {
-						$get_content = apply_filters( 'oceanwp_topbar_template_content', do_blocks( $get_content ) );
-					}
-
-					// Display template content.
-					echo do_shortcode( $get_content );
-
-				}
-				?>
-
-			</div>
-
-			<?php
-		} else {
-			?>
-
-			<?php
-			// Check if there is content for the topbar.
-			if ( !empty($content)
-				|| is_customize_preview() ) :
-				?>
-
-				<span class="topbar-content">
-
-					<?php
-					// Display top bar content from Customizer.
-					if ( !empty($content) ) {
-						echo do_shortcode( $content );
-					}
-					?>
-
-					<!-- 🔥 Custom Hardcoded Contact Info -->
-					<span class="top-phone">📞 +260 97 123 4567</span>
-					<span class="top-email">✉️ sales@shopinzed.com</span>
-
-				</span>
-
-				<?php
-			endif;
-
-		}
-		?>
-
-	</div><!-- #top-bar-content -->
-
-<?php endif; ?>
+</div><!-- #top-bar-content -->
--- themes/oceanwp/partials/topbar/layout.php
+++ themes/oceanwp/partials/topbar/layout.php
@@ -5,27 +5,14 @@
  * @package OceanWP WordPress theme
  */
 
-// Exit if accessed directly.
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+if ( ! defined( 'ABSPATH' ) ) exit;
 
 // Classes.
 $classes = array( 'clr' );
-
-// Add container class if the top bar is not full width.
 if ( true !== get_theme_mod( 'ocean_top_bar_full_width', false ) ) {
 	$classes[] = 'container';
 }
-
-// If no content.
-if ( ! get_theme_mod( 'ocean_top_bar_content' ) ) {
-	$classes[] = 'has-no-content';
-}
-
-// Turn classes into space seperated string.
 $classes = implode( ' ', $classes );
-
 ?>
 
 <?php do_action( 'ocean_before_top_bar' ); ?>
@@ -39,13 +26,8 @@ $classes = implode( ' ', $classes );
 		<div id="top-bar-inner" class="clr">
 
 			<?php
-			// Get content.
+			// Always load your topbar content
 			get_template_part( 'partials/topbar/content' );
-
-			// Get social.
-			if ( true === get_theme_mod( 'ocean_top_bar_social', true ) ) {
-				get_template_part( 'partials/topbar/social' );
-			}
 			?>
 
 		</div><!-- #top-bar-inner -->
