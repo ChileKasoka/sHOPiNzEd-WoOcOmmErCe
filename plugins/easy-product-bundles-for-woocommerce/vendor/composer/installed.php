cc09c835a64de6f8df461d9eededa0c2acf9b890
Chile <118934314+ChileKasoka@users.noreply.github.com>
2025-09-30 14:34:37 +0200

    products shortcode added to homepage

---
+++ plugins/easy-product-bundles-for-woocommerce/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'asanaplugins/easy-product-bundles-woocommerce',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd377d52f8404aacb6cb25924fe46cc555a8c673c',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'asanaplugins/easy-product-bundles-woocommerce' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd377d52f8404aacb6cb25924fe46cc555a8c673c',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
